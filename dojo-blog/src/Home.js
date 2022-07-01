8ba3beb5a1774046d0e81d05877e5a9e8efc4619
johnpathadan <johnpathadan@gmail.com>
2022-07-01 14:30:46 +0530

    events and functions

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+    const handleClick = (e) =>{
+        console.log('Hello ninjas', e);
+    }
+
     return (
         <div className="homePage">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>  
         </div>
     );
 }
