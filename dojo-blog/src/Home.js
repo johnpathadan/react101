2753e54fb55cffdb8583e23c5b386ae53de6a016
johnpathadan <johnpathadan@gmail.com>
2022-07-01 14:59:05 +0530

    use hooks

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
-    const handleClick = (e) =>{
-        console.log('Hello ninjas', e);
+    
+    const [name, setName] = useState('mario'); 
+    const [age, setAge] = useState(25);
+
+    const handleClick = () =>{
+        setName('luigi');
+        setAge(30);
     }
 
     return (
+        
         <div className="homePage">
             <h2>HomePage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>  
         </div>
     );
